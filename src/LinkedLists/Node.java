80a50007770a775b6803c76988247822cf19bc14
J. Osborne III <J0x3@protonmail.com>
2019-01-13 10:56:17 -0800

    clean code

--- src/LinkedLists/Node.java
+++ src/LinkedLists/Node.java
@@ -1,19 +1,19 @@
 package LinkedLists;
 
 class Node<E> {
-    private E d;
-    private Node<E> n, p;
+    private E data;
+    private Node<E> next, prev;
 
     public Node(E data, Node<E> next, Node<E> prev) {
-        d = data;
-        n = next;
-        p = prev;
+        this.data = data;
+        this.next = next;
+        this.prev = prev;
     }
 
-    public E getData() { return d; }
-    public Node<E> getNext() { return n; }
-    public Node<E> getPrev() { return p; }
-    public void setNext(Node<E> newNext) { n = newNext; }
-    public void setPrev(Node<E> newPrev) { p = newPrev; }
-    public void setData(E element) { d = element; }
+    public E getData() { return data; }
+    public Node<E> getNext() { return next; }
+    public Node<E> getPrev() { return prev; }
+    public void setNext(Node<E> newNext) { next = newNext; }
+    public void setPrev(Node<E> newPrev) { prev = newPrev; }
+    public void setData(E element) { data = element; }
 }
